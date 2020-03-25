18528a1f9519d5125b046e4c4dce2ed6de8fbeb9
Farhan Asif <f.asif2010@gmail.com>
2020-03-26 00:31:55 +0600

    project summery

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from "./components/layout/Navbar";
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Dashboard} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summery">
+            <div className="card-content gray-text text-darken-3">
+                <span className="card-title">Project Title</span>
+                <p>Posted by Farhan Asif</p>
+                <p className="gray-text">3rd Feb, 2020 2am</p>
+            </div>
+        </div>
+    )
+}
