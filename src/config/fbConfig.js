38b6900ab323ca500f8678584560f9da80bcd3b7
Farhan Asif <f.asif2010@gmail.com>
2020-03-26 13:31:30 +0600

    configuring firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: REACT_APP_API_KEY,
+    authDomain: "react-redux-c02aa.firebaseapp.com",
+    databaseURL: "https://react-redux-c02aa.firebaseio.com",
+    projectId: "react-redux-c02aa",
+    storageBucket: "react-redux-c02aa.appspot.com",
+    messagingSenderId: "662703363063",
+    appId: "1:662703363063:web:77d8bf71047094ca09d4f2",
+    measurementId: "G-0NYDD7N72Y"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+firebase.firestore();
+
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createFirestoreInstance, reduxFirestore, getFirestore } from 'redux-firestore';
+import { ReactReduxFirebaseProvider, reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import firebase from 'firebase/app'
+import fbConfig from './config/fbConfig'
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore}))
+    // reactReduxFirebase(fbConfig), // redux binding for firebase
+    // reduxFirestore(fbConfig) // redux bindings for firestore
+  )
+);
+
+const rrfProps = {
+     firebase,
+     config: fbConfig,
+     dispatch: store.dispatch,
+     createFirestoreInstance // <- needed if using firestore
+}
 
 ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode></Provider>,
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
