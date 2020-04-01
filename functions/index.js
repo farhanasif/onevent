6d9bac94e27895f097d3ff1b3433446bfbb87b1d
Farhan Asif <f.asif2010@gmail.com>
2020-04-01 14:43:10 +0600

    functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +9,7 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+// exports.projectCreated = functions.firestore.document('projects/{projectId}').onCreate((doc) => {
+    
+// })
