226862a99c246b36f1c6050585a4f0bbf5977866
Farhan Asif <f.asif2010@gmail.com>
2020-04-02 22:09:57 +0600

    function update

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,20 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
 
+const createNotification = (notification) => {
+    return admin.firestore().collection('notifications').add(notification)
+    .then(doc => {
+        console.log('notification added', doc)
+    })
+}
+
 exports.projectCreated = functions.firestore.document('projects/{projectId}').onCreate((doc) => {
-    
+    const project = doc.data();
+    const notification = {
+        content: 'Added a new project',
+        user: `${project.authorFirstName} ${project.authorLastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp()
+    }
+
+    return createNotification(notification)
 })
