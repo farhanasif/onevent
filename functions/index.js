b3a2a7010d6fa7e77931a93f2643dbe55843cafe
Md Farhan Asif <f.asif2010@gmail.com>
2020-04-04 15:32:08 +0600

    cloud function update

--- functions/index.js
+++ functions/index.js
@@ -2,28 +2,15 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-const createNotification = (notification) => {
-    return admin.firestore().collection('notifications').add(notification)
-    .then(doc => {
-        console.log('notification added', doc)
-    })
-}
-
 exports.projectCreated = functions.firestore.document('projects/{projectId}').onCreate((doc) => {
     const project = doc.data();
-    const notification = {
-        content: 'Added a new project',
+    return admin.firestore().collection('notifications').add({
+        content: 'added a new project',
         user: `${project.authorFirstName} ${project.authorLastName}`,
         time: admin.firestore.FieldValue.serverTimestamp()
-    }
-
-    return createNotification(notification)
+    })
+    .then(doc => {
+        //console.log('notification added', doc)
+        return doc;
+    })
 })
